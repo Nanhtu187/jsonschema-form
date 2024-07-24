7057da413e1774f4d0d89b1d9e674b65a660a902
Nanhtu187 <nguyenanhtuchy2003@gmail.com>
2024-07-24 15:08:32 +0700

    update code

--- lib/utils/src/schema/loadFormData.ts
+++ lib/utils/src/schema/loadFormData.ts
@@ -1,23 +1,30 @@
-import {Schema, StrictSchema} from "../types.ts";
-import {ARRAY_TYPE, DEFAULT_KEY, PROPERTIES_KEY, TYPE_KEY} from "../constants.ts";
-import {get, set} from 'lodash';
+import { Schema, StrictSchema } from '../types.ts';
+import {
+  ARRAY_TYPE,
+  DEFAULT_KEY,
+  PROPERTIES_KEY,
+  TYPE_KEY,
+} from '../constants.ts';
+import get from 'lodash/get';
+import set from 'lodash/set';
 
 export function getFormData<T = any, S extends StrictSchema = Schema>(
-    schema: S,
+  schema: S,
 ): T {
-    if(DEFAULT_KEY in schema) {
-        return get(schema, [DEFAULT_KEY]) as T
+  if (DEFAULT_KEY in schema) {
+    return get(schema, [DEFAULT_KEY]) as T;
+  }
+  if (TYPE_KEY in schema && get(schema, [TYPE_KEY]) == ARRAY_TYPE) {
+    return [] as T;
+  }
+  if (PROPERTIES_KEY in schema) {
+    let formData = {};
+    const properties = get(schema, [PROPERTIES_KEY]);
+    for (const property in properties) {
+      const field: any = get(properties, [property]);
+      set(formData, [property], getFormData(field));
     }
-    if(TYPE_KEY in schema && get(schema, [TYPE_KEY]) == ARRAY_TYPE) {
-        return [] as T
-    }
-    if(PROPERTIES_KEY in schema) {
-        let formData = {}
-        for(const property in schema.properties) {
-            const field = get(schema, [PROPERTIES_KEY, property]);
-            set(formData, [property], getFormData(field as S));
-        }
-        return formData as T
-    }
-    return "" as T
+    return formData as T;
+  }
+  return '' as T;
 }
